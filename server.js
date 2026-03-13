daf97e6dab4ab7ea1a3516c4ebd0bf6aec51b731
omega-nugget007 <maxi.emy8@gmail.com>
2026-03-13 20:48:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => res.json({ status: "ok" }));
+
+
 // ---------- CONFIG ----------
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const DISCORD_GUILD_ID = process.env.DISCORD_GUILD_ID;
